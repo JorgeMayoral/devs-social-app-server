b0a95a7c55f484a7ca2419801806ea8a7c15e5c1
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-03-10 11:26:56 +0100

    feat: :sparkles: Added database connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
@@ -7,6 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const MONGO_URI = process.env.MONGO_URI;
+
+connectDB(MONGO_URI);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
