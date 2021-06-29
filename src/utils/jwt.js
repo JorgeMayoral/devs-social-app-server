5d97e19824006d884372fbe960969cafc3b5be90
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-06-29 19:29:49 +0200

    feat: :sparkles: Added JWT functions
    Added functions to generate and validate jsonwebtokens

---
+++ src/utils/jwt.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(id, username) {
+  console.log(id, username);
+  if (id === undefined || username === undefined) {
+    return null;
+  }
+
+  const secret = process.env.JWT_SECRET_KEY;
+  const expirationDate = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365; // Token expires in 1 year
+  const payload = {
+    id: id,
+    username: username,
+    exp: expirationDate,
+  };
+  const token = jwt.sign(payload, secret);
+  return token;
+}
+
+function validateToken(token) {
+  const secret = process.env.JWT_SECRET_KEY;
+  let data;
+  try {
+    data = jwt.verify(token, secret);
+  } catch (err) {
+    data = { error: err };
+  }
+
+  return data;
+}
+
+module.exports = { generateToken, validateToken };
